3a7daca1c23de4c8ef00635e86fb43cc4bd6c2ce
Philip J <philip.jasionowski@gmail.com>
2020-10-17 18:45:42 -0400

    Need to fix looping

--- NatrlFoundationsProject2/swag.cpp
+++ NatrlFoundationsProject2/swag.cpp
@@ -1,60 +1,95 @@
 #include <iostream>;
 #include <string>;
-#include <cstring>;
+#include <cstring>;	
 using namespace  std;
 
 
 int main() {
 	//create char array
-	char str[128];
-	//create int i used in loops and x which indicates our current char as an int value
-	int i = 0, x = 0;
+
+	//create int i used in loops and x which indicates our current char as an int value, n is the encryption/decryption number
+	int i = 0, x = 0, n = 0;
+	bool done = false;
+	string yesno, final, test1, cont ="C";
 	//take string (create a code to take string as input)
-	string test1 = "This is a test string";
-
-	//**CREATE A CODE TO PICK IF WE ARE ENCRYPTING OR DECRYPTING**
-
-
-
-
-	//take encryption number (create code to take encryption number)
-	int n = 2;
-
-	for (i = 0; ((i < 128) && (str[i] !='\0')) ; i++)	//checks if the character at a given position is a null character
+	while (cont=="C")
 	{
-		//sets x to the ASCII value of char
-		x = str[i];
-		if (x<91 && x>64)			//checks if it is upercase using ASCII values, if not it then checks for lowercase
+		cout << "Please input the string you would like to encrypt/decrypt\n";
+		getline(cin, test1);	//gets next line and inputs it as the test1 string
+		while (!((yesno == "E" || yesno == "D") && done))
 		{
-			if (x+n>90)				//checks if char would go out of bounds (upercase runs from 64-90)
+			cout << "would you like to encrypt or decrypt? (E/D)\n";
+			getline(cin, yesno);
+			//if E is input it uses the following encryption code
+			if (yesno == "E" || yesno == "D")
 			{
-				str[i] = ((x + n) % 90 + 64);			//uses the remainder function to only leave a leftover above 90 and then adds 64 to get the minimum number
-			}
-			else
-			{
-				str[i] = x + n;
-			}
+				if (yesno == "E")
+				{
+					cout << "what is the encryption key you would like to use?\n";
+					cin >> n;
+					//makes n smaller 
+					n = n % 26;
+					//cout << n << "\n";
+				}
+				else if (yesno == "D")
+				{
+					cout << "what is the decryption key you would like to use?\n";
+					cin >> n;
+					n = (26 - (n % 26));
+					//cout << n << "\n";
 
+				}
+				for (i = 0; i < test1.length(); i++)	//checks if the character at a given position is a null character
+				{
+					//sets x to the ASCII value of char
+					x = test1[i];
+					//checks if it is upercase using ASCII values, if not it then checks for lowercase
+					if (x < 91 && x>64)
+					{
+						//checks if char would go out of bounds (upercase runs from 64-90)
+						if (x + n > 90)
+
+						{
+							// uses the remainder function to only leave a leftover above 90 and then adds 64 to get the minimum number
+							final.push_back(((x + n) % 90 + 64));
+						}
+						else
+						{
+							final.push_back(x + n);
+						}
+
+					}
+					//checks if it is lowercase using ASCII values, if not then the char will be unafected
+					else if (x < 123 && x > 96)
+					{
+						//checks if char would go out of bounds (lowercase runs from 96-122)
+						if (x + n > 122)
+						{
+							//uses the remainder function to only leave a leftover above 122 and then adds 96 to get the minimum number
+							final.push_back((x + n) % 122 + 96);
+						}
+						else
+						{
+							final.push_back(x + n);
+						}
+
+					}
+					//if the ASCII value is none of the above, it is a special character of sorts and will not be affected
+					else
+					{
+						final.push_back(x);
+					}
+				}
+				done = true;
+				break;
+			}
 		}
-		else if ((int)str[i] < 123 && (int)str[i]>96)	//checks if it is lowercase using ASCII values, if not then the char will be unafected
+		cout << final << "\n";
+		cout << "Select an option\n (E) Exit\n (C) Continue Encrypting/Decrypting\n";
+		getline(cin, cont);
+		if (cont=="E")
 		{
-			if (x + n > 122)		//checks if char would go out of bounds (lowercase runs from 96-122)
-			{
-				str[i] = ((x + n) % 122 + 96);			//uses the remainder function to only leave a leftover above 122 and then adds 96 to get the minimum number
-			}
-			else
-			{
-				str[i] = x + n;
-			}
-
+			exit;
 		}
-
 	}
-	//add encryption number to each char
-
-	//if an int goes over the max, subtract it from the int and add the min
-
-	//convert back to char array
-
-	//convert back to string
 }
